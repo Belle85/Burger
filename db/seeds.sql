70bea1caa03de5dab6b6fc3956b89930d516f0f7
Belle <bebold.bebelle@gmail.com>
2018-08-23 09:12:41 -0400

    Created my seeds and ran both schema and seeds in my phpMyAdmin.

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("The Frenchy");
+
+INSERT INTO burgers (burger_name)
+VALUES ("The Big Apple");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Welcome to SoFlo");
+
+INSERT INTO burgers (burger_name)
+VALUES ("The Triple X");
